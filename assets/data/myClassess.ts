9ae1041a740294d8275a5addcbfcd176447ed8a8
JainSheridan <50390799+JainSheridan@users.noreply.github.com>
2019-07-27 20:47:39 -0400

    Add files via upload

---
+++ assets/data/myClassess.ts
@@ -0,0 +1,6 @@
+import { ClassInfo } from '../../app/ClassInfo';
+
+export const MYCLASSESS: ClassInfo[] = [
+    { cname : 'SYST24444',room:'J303',dow:'Mon',tod:'10-1'},
+    { cname : 'SYST35300',room:'H210',dow:'Fri',tod:'8-11'}
+];
